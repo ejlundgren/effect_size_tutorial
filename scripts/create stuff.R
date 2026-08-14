c2c46e6188e08379c5e4b9c3dd44b0df79f9c035
Erick Lundgren <erick.lundgren@gmail.com>
2026-08-13 20:30:33 -0600

    updates

---
+++ scripts/create stuff.R
@@ -0,0 +1,15 @@
+#
+#
+# Create a silly hex
+#
+#
+library("hexSticker")
+
+file.exists("figures/illustration_icons/doggo.png")
+file.exists("figures/flowcharts_diagrams/temp_logo.png")
+
+imgurl <- system.file("figures/illustration_icons/doggo.png")
+sticker("figures/illustration_icons/doggo.png", 
+        h_fill = "magenta1",
+        package="Effect This!", p_size=18, s_x=1, s_y=.75, s_width=.5,
+        filename="figures/flowcharts_diagrams/temp_logo.png")
